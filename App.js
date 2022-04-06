6bd92eb9fadf01e3755a55830a0e5938483c84f9
Nazmul Hasan <najmul.hasan10@gmail.com>
2022-04-06 12:10:10 +0600

    google sign in configured for ios

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
-
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 const App = () => {
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfo, setuserInfo] = useState([]);
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email'],
+      webClientId:
+        '925749541564-b22a8mpdspl1ju1u8m3a5cmrvg1ndvh6.apps.googleusercontent.com',
+      offlineAccess: true,
+    });
+  }, []);
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {accessToken, idToken} = await GoogleSignin.signIn();
+      setloggedIn(true);
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        alert('Cancel');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        alert('Signin in progress');
+        // operation (f.e. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        alert('PLAY_SERVICES_NOT_AVAILABLE');
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity>
-        <Text>Login</Text>
-      </TouchableOpacity>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+      />
     </SafeAreaView>
   );
 };
