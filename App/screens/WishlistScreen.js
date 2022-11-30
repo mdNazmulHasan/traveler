c769de4094846dbd36a0eb1f537d40c24e276ac7
mdNazmulHasan <najmul.hasan10@gmail.com>
2022-11-30 11:52:19 +0600

    fab icon added

--- App/screens/WishlistScreen.js
+++ App/screens/WishlistScreen.js
@@ -1,12 +1,45 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import {AnimatedFAB} from 'react-native-paper';
 
 function WishlistScreen() {
+  const [isExtended, setIsExtended] = React.useState(true);
+  const onScroll = ({nativeEvent}) => {
+    const currentScrollPosition =
+      Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
+
+    setIsExtended(currentScrollPosition <= 0);
+  };
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView onScroll={onScroll}>
+        {[...new Array(100).keys()].map((_, i) => (
+          <Text>{i}</Text>
+        ))}
+      </ScrollView>
+      <AnimatedFAB
+        icon={'plus'}
+        label={'Add New'}
+        extended={isExtended}
+        onPress={() => console.log('Pressed')}
+        visible
+        animateFrom={'right'}
+        iconMode={'dynamic'}
+        style={styles.fabStyle}
+      />
+    </SafeAreaView>
   );
 }
 
 export default WishlistScreen;
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+  },
+  fabStyle: {
+    bottom: 16,
+    right: 16,
+    position: 'absolute',
+  },
+});
