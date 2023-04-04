92cff5ed14c989ad3d65e8de418b1c2563ab5bc8
mdNazmulHasan <najmul.hasan10@gmail.com>
2023-04-04 12:39:40 +0600

    added new screen to add place in screen index

--- App/screens/index.js
+++ App/screens/index.js
@@ -1,5 +1,6 @@
 import LoginScreen from './LoginScreen';
 import WishlistScreen from './WishlistScreen';
 import AppLoadingScreen from './AppLoadingScreen';
+import AddNewPlaceScreen from './AddNewPlaceScreen';
 
-export {LoginScreen, WishlistScreen, AppLoadingScreen};
+export {LoginScreen, WishlistScreen, AppLoadingScreen, AddNewPlaceScreen};
