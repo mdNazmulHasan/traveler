03af9c3b0e3b1762f94835736eca33f7d785528f
mdNazmulHasan <najmul.hasan10@gmail.com>
2023-04-04 12:39:18 +0600

    created new screen to add place

---
+++ App/screens/AddNewPlaceScreen.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const AddNewPlaceScreen = () => {
+  return (
+    <View>
+      <Text>AddNewPlaceScreen</Text>
+    </View>
+  );
+};
+
+export default AddNewPlaceScreen;
+
+const styles = StyleSheet.create({});
